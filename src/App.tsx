4c48ef43f1f51be5a983d95e1ca5a76de73c46de
withoutspaces <dms@ic.ufal.br>
2023-11-24 20:15:35 -0300

    refactor: change name of the state

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { useState } from "react"
-import { Container, ButtonContainer } from "./App.styles"
-import Calendar from "./components/Calendar"
-import Button from "./components/Button"
+import { useState } from "react";
+import { Container, ButtonContainer, CalendarContainer } from "./App.styles";
+import Calendar from "./components/Calendar";
+import Button from "./components/Button";
+import EventForm from "./components/EventForm";
+
+import { IEvents } from "./@types/events";
+
+
 
 
 function App() {
+  const [month, setMonth] = useState(1);
+  const [events, setEvents] = useState<IEvents[]>([])
+ 
+  const daysWithEvents = events.length > 0 ? events.map(event => event.date) : null
 
-  const [month, setMonth] = useState(1)
-
-  function handleButton(n: number){
-    const num = month + n
-    if(!num) return
-    if(num <= 12) {
-      setMonth(num)
+  function handleButton(n: number) {
+    const num = month + n;
+    if (!num) return;
+    if (num <= 12) {
+      setMonth(num);
     }
-    return
+    return;
   }
 
   return (
     <Container>
-      <Calendar monthId={month}/>
-      <ButtonContainer>
-        <Button onClick={() => handleButton(-1)}>Anterior</Button>
-        <Button onClick={() => handleButton(1)}>Próximo</Button>
-      </ButtonContainer>
+      <EventForm setEvent={setEvents}/> 
+      <CalendarContainer>
+        <Calendar monthId={month} daysWithEvents={daysWithEvents} />
+        <ButtonContainer>
+          <Button onClick={() => handleButton(-1)}>Anterior</Button>
+          <Button onClick={() => handleButton(1)}>Próximo</Button>
+        </ButtonContainer>
+      </CalendarContainer>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
