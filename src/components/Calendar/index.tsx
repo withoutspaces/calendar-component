b5b252a1b32c414fe761f57944ff496848392426
withoutspaces <dms@ic.ufal.br>
2023-11-24 21:16:29 -0300

    feat: add event state

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,28 +1,36 @@
 import CalendarButton from "./components/CalendarButton/index.tsx";
 import CalendarSpaces from "./components/CalendarSpaces/index.tsx";
 import DaysOfTheWeek from "./components/DaysOfTheWeek/index.tsx";
-
-import { CalendarContainer, Title } from "../../App.styles.ts";
+import { CalendarContainer, Title, Container } from "./styles.ts";
 import { calendar } from "../../mocks/calendar.ts";
 import { CalendarProps } from "./types.ts";
 
 
-export default function Calendar({ monthId }: CalendarProps) {
+export default function Calendar({ monthId, daysWithEvents }: CalendarProps) {
   const months = calendar.filter((month) => month.monthNumber === monthId)[0];
 
   return (
-    <>
+    <Container>
       <Title>{months.name}</Title>
       <DaysOfTheWeek />
       <CalendarContainer>
         {months.days.map((day, index) => {
+          const currentDate = `2023-${
+            months.monthNumber < 10
+              ? "0" + months.monthNumber
+              : months.monthNumber
+          }-${day < 10 ? "0" + day : day}`;
+
+          const hasEvent = daysWithEvents?.includes(currentDate);
+          
+
           if (index === 0 && months.firstDay) {
             const { firstDay } = months;
             return Array.from({ length: firstDay }, () => <CalendarSpaces />);
           }
-          return <CalendarButton name={day} />;
+          return <CalendarButton name={day} hasEvent={hasEvent} />;
         })}
       </CalendarContainer>
-    </>
+    </Container>
   );
 }
