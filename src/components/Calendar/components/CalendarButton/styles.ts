cce1e591ade1c4d2831044a6dc66ce4f790fd377
withoutspaces <dms@ic.ufal.br>
2023-11-25 09:17:09 -0300

    style: add visual effects to event button

--- src/components/Calendar/components/CalendarButton/styles.ts
+++ src/components/Calendar/components/CalendarButton/styles.ts
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
-
-export const DayButton = styled.button<{ $event?: boolean}>`
-    background-color: ${props => props.$event ? "#443c68" : "gray"};
+export const DayButton = styled.button<{ $event?: boolean }>`
+  background-color: ${(props) => (props.$event ? "#443c68" : "gray")};
+  
+  border: none;
+  width: 64px;
+  height: 64px;
+  &:hover {
+    opacity: .8;
+  }
+  &:active {
+    background-color: ${(props) => (props.$event ? "#352f52" : "#3e3d40")};
+  }
+  &:focus {
+    border: ${(props) => (props.$event ? "1px solid white" : "")};
     
-    border: none;
-    width: 64px;
-    height: 64px;
-`
+  }
+`;
